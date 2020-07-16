83444cbd3e5c5785d767adc74d1eacb78bd1877d
tarun-sharma03 <tarundixit0310@gmail.com>
2020-07-16 14:01:51 +0530

    Post improved, Comments pending

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Image, Text, StyleSheet } from "react-native";
-import { Card } from "react-native-paper";
+import { Card, Avatar } from "react-native-paper";
 import { Entypo, AntDesign, FontAwesome } from "@expo/vector-icons";
 
 export default class Post extends React.Component {
@@ -12,16 +12,7 @@ export default class Post extends React.Component {
             title="tarun_sharma03"
             titleStyle={{ fontSize: 15, color: "white" }}
             left={() => (
-              <View
-                style={{
-                  width: 50,
-                  height: 50,
-                  borderRadius: 25,
-                  backgroundColor: "grey",
-                  marginLeft: -5,
-                  marginRight: 5,
-                }}
-              />
+              <Avatar.Image size={40} source={require("../assets/face.jpg")} />
             )}
             right={() => (
               <Entypo name="dots-three-vertical" size={24} color="white" />
@@ -46,35 +37,38 @@ export default class Post extends React.Component {
           />
         </View>
         <View style={{ flexDirection: "row" }}>
-          <AntDesign
-            name="heart"
-            size={30}
-            color="red"
-            style={{ marginTop: 10, marginLeft: 20, marginRight: 10 }}
-          />
-          <FontAwesome
-            name="comment-o"
-            size={30}
-            color="white"
-            style={{ marginTop: 7, marginRight: 10 }}
-          />
+          <View style={{ flexDirection: "row", flex: 1 }}>
+            <AntDesign
+              name="heart"
+              size={30}
+              color="red"
+              style={{ marginTop: 10, marginLeft: 20, marginRight: 10 }}
+            />
+            <FontAwesome
+              name="comment-o"
+              size={30}
+              color="white"
+              style={{ marginTop: 7, marginRight: 10 }}
+            />
 
-          <Entypo
-            name="direction"
-            size={30}
-            color="white"
-            style={{ marginTop: 7, marginRight: 10 }}
-          />
-
-          <FontAwesome
-            name="bookmark-o"
-            size={30}
-            color="white"
-            style={{ marginTop: 7, marginLeft: 230 }}
-          />
+            <Entypo
+              name="direction"
+              size={30}
+              color="white"
+              style={{ marginTop: 7, marginRight: 10 }}
+            />
+          </View>
+          <View>
+            <FontAwesome
+              name="bookmark-o"
+              size={30}
+              color="white"
+              style={{ marginTop: 7, marginRight: 10 }}
+            />
+          </View>
         </View>
-        <Text style={styles.text}>Liked By You and 5,000 others</Text>
-        <View style={{ flexDirection: "row" }}>
+        <Text style={styles.text}>Liked By You and 10,000 others</Text>
+        <View style={{ flexDirection: "row", flex: 1 }}>
           <Text style={{ ...styles.text, fontWeight: "bold" }}>
             tarun_sharma03
           </Text>
